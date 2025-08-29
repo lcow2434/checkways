796166d5a9bae37a0e14666e6d0baa577f6a5a6c
Archit Parikh <160279085+lcow2434@users.noreply.github.com>
2025-08-29 02:31:14 -0700

    3rd commit

---
+++ StringBuilder.java
@@ -0,0 +1,13 @@
+public class StringBuilder {
+    public static void main(String[]args){
+       java.lang.StringBuilder sb= new java.lang.StringBuilder("h");
+       sb.append("e");
+       sb.append("l");
+       sb.append("l");
+       sb.append("o");
+
+        System.out.println(sb);
+
+    }
+
+}
