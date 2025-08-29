19bee68bcaed9ac1b45cc91b3ab987affc879650
Archit Parikh <160279085+lcow2434@users.noreply.github.com>
2025-08-29 02:20:37 -0700

    initial commit

---
+++ Array.java
@@ -0,0 +1,11 @@
+public class Array {
+    public static void main(String[]args){
+       int i;
+        int[] array={1,2,3,4,5,6};
+        for (i=0;i<array.length;i++){
+            System.out.println(array[i]);
+        }
+
+    }
+
+}
